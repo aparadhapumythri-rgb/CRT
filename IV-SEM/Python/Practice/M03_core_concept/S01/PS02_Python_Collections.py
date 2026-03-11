ef4313fa34a6bc1cc081566667b76533e640f322
Mythri <aparadhapumythri@gmail.com>
2026-03-11 18:42:05 +0530

    message

---
+++ IV-SEM/Python/Practice/M03_core_concept/S01/PS02_Python_Collections.py
@@ -0,0 +1,17 @@
+'''
+sets:
+1.definition 
+2.
+3.Adding -->
+4.removing -->
+5.set operations -->
+6.leetcode problems on sets
+'''
+a =set([10,20,30,40,50])
+print(a)
+a.add(78)
+print(a)
+a.remove(20)
+print(a)
+a.discard(30)
+print(a)
